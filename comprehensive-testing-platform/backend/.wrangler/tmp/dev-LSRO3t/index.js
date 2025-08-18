29bcca2e9d920dcf5a3242d3c60bee0c725e6b2c
Rheali233 <cyberlina@163.com>
2025-08-19 00:38:16 +0800

    test: trigger automatic deployment with configured secrets

--- comprehensive-testing-platform/backend/.wrangler/tmp/dev-LSRO3t/index.js
+++ comprehensive-testing-platform/backend/.wrangler/tmp/dev-LSRO3t/index.js
@@ -5,7 +5,7 @@ var __export = (target, all) => {
     __defProp(target, name, { get: all[name], enumerable: true });
 };
 
-// .wrangler/tmp/bundle-FY8UX0/checked-fetch.js
+// .wrangler/tmp/bundle-f56PAZ/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -11989,7 +11989,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-FY8UX0/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-f56PAZ/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -12021,7 +12021,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-FY8UX0/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-f56PAZ/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
